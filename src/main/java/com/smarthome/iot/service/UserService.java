d2a910570ee006f7941c8dfa7f496a10edf3d0e7
hoanganh04-11 <ht.anh00411@gmail.com>
2026-03-25 19:00:22 +0700

    first commit project

---
+++ src/main/java/com/smarthome/iot/service/UserService.java
@@ -0,0 +1,11 @@
+package com.smarthome.iot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    public String handleHello(){
+        return "Hello from service";
+    }
+}
